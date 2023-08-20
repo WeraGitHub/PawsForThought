a2b33fbb96989a0b064f3c92e1082420c0ef31d5
WeraGitHub <weronikalimberger@gmail.com>
2023-08-20 13:52:31 +0100

    Toggle arrow up / down when clicked to expand / minimise post content

--- application/static/script.js
+++ application/static/script.js
@@ -5,13 +5,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Loop through each arrow element
   arrows.forEach(function(arrow) {
-    // Find the corresponding target element for this arrow
-    const targetElement = arrow.parentElement.querySelector('.inner-post');
-
+    // Find the post element that contains this arrow
+    const post = arrow.closest('.post');
+    // Find the corresponding target element within the post
+    const targetElement = post.querySelector('.inner-post');
     // Add a click event listener to the arrow element
     arrow.addEventListener('click', function() {
       // Toggle the desired class on the target element
       targetElement.classList.toggle('display');
+
+      // Toggle the src attribute of the arrow image
+      const altSrc = arrow.getAttribute('data-alt-src');
+      if (altSrc) {
+        const currentSrc = arrow.getAttribute('src');
+        arrow.setAttribute('src', altSrc);
+        arrow.setAttribute('data-alt-src', currentSrc);
+      }
     });
+
   });
+
 });
