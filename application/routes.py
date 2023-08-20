79f30d70cc76298c7d4b3bc8e8aac8c127850cd2
WeraGitHub <weronikalimberger@gmail.com>
2023-08-20 17:01:54 +0100

    Create and style About and Contact pages

--- application/routes.py
+++ application/routes.py
@@ -8,4 +8,17 @@ from application.posts_repo import posts
 @app.route('/index')
 def home():
     header = "Paws For Thought"
-    return render_template('home.html', header=header, posts=posts)
+    sub_header = "reflective blog"
+    return render_template('home.html', header=header, subheader=sub_header, posts=posts)
+
+
+@app.route('/about')
+def about():
+    header = "About"
+    return render_template('about.html', header=header, subheader='')
+
+
+@app.route('/contact')
+def contact():
+    header = "Contact"
+    return render_template('contact.html', header=header, subheader='')
