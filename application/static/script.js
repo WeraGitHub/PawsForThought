6bcd2d43f2c1b7297368755cf048335818fa48fd
WeraGitHub <weronikalimberger@gmail.com>
2023-08-20 16:19:33 +0100

    Make images clickable to display them in a bigger version in a modal

--- application/static/script.js
+++ application/static/script.js
@@ -25,4 +25,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
   });
 
+
+  // Get references to the clickable images, modal, modal image, and close button
+  const clickableImages = document.querySelectorAll('.clickable-image');
+  const modal = document.getElementById('modal');
+  const modalImage = document.getElementById('modal-image');
+  const closeButton = document.querySelector('.close-button');
+
+  // Add click event listeners to each clickable image
+  clickableImages.forEach(function(image) {
+    image.addEventListener('click', function() {
+      // Set the source of the modal image to the clicked image's source
+      modalImage.src = image.getAttribute('src');
+      // Display the modal
+      modal.style.display = 'block';
+    });
+  });
+
+  // Add click event listener to the close button
+  closeButton.addEventListener('click', function() {
+    // Hide the modal when the close button is clicked
+    modal.style.display = 'none';
+  });
+
+  // Add click event listener to the window
+  window.addEventListener('click', function(event) {
+    // Close the modal if the user clicks outside the modal content
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+
 });
